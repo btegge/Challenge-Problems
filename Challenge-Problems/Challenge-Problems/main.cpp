3b62e405d2ca077b597fa5b3302c8b326d5ed2cd
Ben Tegge <benjamin.tegge@gmail.com>
2022-10-25 20:50:59 -0700

    Initial Commit

---
+++ Challenge-Problems/Challenge-Problems/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Challenge-Problems
+//
+//  Created by Benjamin Tegge on 10/25/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
